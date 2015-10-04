3e25e37bba29d34ee672d29c59ea8b323773ddbb
Emily Martinez <em3021@columbia.edu>
2015-10-04 19:44:12 -0400

    adds jplayer, changes post-title and nav styles

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -24,3 +24,28 @@ window.location.href = "audio";
 });
 
 }
+
+$(document).ready(function() {
+
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function(event) {
+            $(this).jPlayer("setMedia", {
+				title: "Bubble",
+				m4a: "http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
+				oga: "http://jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+            });
+        },
+        // swfPath: "http://jplayer.org/latest/dist/jplayer",
+        supplied: "mp3, oga",
+		volume: 0.8,
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true,
+		remainingDuration: true,
+		toggleDuration: true
+    });
+});                                      
+
+
