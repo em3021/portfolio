dcb87affcb18b92b4cd047124a7dd2b0d368f77b
Emily Martinez <em3021@columbia.edu>
2015-11-17 13:52:56 -0500

    adds playlist addon, playlist js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,75 +37,79 @@ if ($(this).scrollTop() > 1){
 
 // }
 
-$(document).ready(function() {
+
+
+
+$(document).ready(function(){
+
+  new jPlayerPlaylist({
+    jPlayer: "#jquery_jplayer_1",
+    cssSelectorAncestor: "#jp_container_1"
+  }, [
+    {
+      title:"Cro Magnon Man",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+    },
+    {
+      title:"Your Face",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+    },
+    {
+      title:"Cyber Sonnet",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+    },
+    {
+      title:"Tempered Song",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
+    },
+    {
+      title:"Hidden",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+    },
+    {
+      title:"Lentement",
+      free:true,
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
+    }
+  ], {
+    swfPath: "http://jplayer.org/latest/dist/jplayer",
+    supplied: "oga, mp3",
+    wmode: "window",
+    useStateClassSkin: true,
+    autoBlur: false,
+    smoothPlayBar: true,
+    keyEnabled: true
+  });
+
+  $("#jplayer_inspector_1").jPlayerInspector({jPlayer:$("#jquery_jplayer_1")});
+});
+
+// $(document).ready(function() {
  
-    $("#jquery_jplayer_1").jPlayer({
-        ready: function(event) {
-            $(this).jPlayer("setMedia", {
-				title: "Bubble",
-				m4a: "http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
-				oga: "http://jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-            });
-        },
-        swfPath: "http://jplayer.org/latest/dist/jplayer",
-        supplied: "mp3, oga",
-		volume: 0.8,
-		// wmode: "window",
-		useStateClassSkin: true,
-		autoBlur: false,
-		smoothPlayBar: true,
-		keyEnabled: true,
-		remainingDuration: true,
-		toggleDuration: true
-    });
-});  
+//     $("#jquery_jplayer_1").jPlayer({
+//         ready: function(event) {
+//             $(this).jPlayer("setMedia", {
+//         title: "Bubble",
+//         m4a: "http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3",
+//         oga: "http://jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+//             });
+//         },
+//         swfPath: "http://jplayer.org/latest/dist/jplayer",
+//         supplied: "mp3, oga",
+//     volume: 0.8,
+//     // wmode: "window",
+//     useStateClassSkin: true,
+//     autoBlur: false,
+//     smoothPlayBar: true,
+//     keyEnabled: true,
+//     remainingDuration: true,
+//     toggleDuration: true
+//     });
+// });  
 
-
-
-// var myPlaylist = new jPlayerPlaylist({
-//   jPlayer: "#jquery_jplayer_1",
-//   cssSelectorAncestor: "#jp_container_1"
-// }, [
-//   {
-//     title:"Cro Magnon Man",
-//     artist:"The Stark Palace",
-//     mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-//     oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
-//     poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
-//   },
-//   {
-//     title:"Hidden",
-//     artist:"Miaow",
-//     free: true,
-//     mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
-//     oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
-//     poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
-//   }
-// ], {
-//   playlistOptions: {
-//     enableRemoveControls: true
-//   },
-//   swfPath: "/js",
-//   supplied: "ogv, m4v, oga, mp3",
-//   smoothPlayBar: true,
-//   keyEnabled: true,
-//   audioFullScreen: true // Allows the audio poster to go full screen via keyboard
-// });
-
-// myPlaylist.setPlaylist ([
-//   {
-//     title:"Cro Magnon Man",
-//     artist:"The Stark Palace",
-//     mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-//     oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
-//     poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
-//   },
-//   {
-//     title:"Hidden",
-//     artist:"Miaow",
-//     free: true,
-//     mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
-//     oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
-//     poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
-//   }
-// ]);
