32adc3de50f1dc1985d70beecd22a5ddf6e45e47
Emily Martinez <em3021@columbia.edu>
2015-11-19 18:05:19 -0500

    edits playlist js, adds intros

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -48,23 +48,28 @@ $(document).ready(function(){
   }, [
     {
       title:"\'I\'m So Proud To Have You As A Daughter\'",
-            mp3:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter.mp3"
+      mp3:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter.mp3",
+      intro:'“An immigrant from Haiti talks with his high-IQ daughter about his love for her, and why he works so hard to provide for her education.”'
     },
     {
       title:"Dad Aches For Son Killed By Policeman 20 Years Ago",
-      mp3:"/portfolio/assets/audio/20150515_me_dad_aches_for_tween_son_killed_by_policeman_20_years_ago.mp3"
+      mp3:"/portfolio/assets/audio/20150515_me_dad_aches_for_tween_son_killed_by_policeman_20_years_ago.mp3",
+      intro:'“Thirteen-year-old Nicholas Heyward Jr. was playing with a toy gun in the stairwell of the housing complex where he lived in Brooklyn when a police officer shot and killed him in 1994.”'
     },
     {
       title:"10 Years Later, A Pair Of Strangers Revisit A Leap Not Taken",
-      mp3:"/portfolio/assets/audio/20150306_me_ten_years_later_two_strangers_revisit_what_might_have_been_lost.mp3"
+      mp3:"/portfolio/assets/audio/20150306_me_ten_years_later_two_strangers_revisit_what_might_have_been_lost.mp3",
+      intro:'“On March 11, 2005, Kevin Berthia wanted to jump off the Golden Gate Bridge. California Highway Patrol Officer Kevin Briggs talked him out of it. The two met recently to discuss what happened that day.”'
     },
     {
       title:"Remembering A \'Giant\': \'Everything We Did, We Did Together\'",
-      mp3:"/portfolio/assets/audio/20150116_me_remembering_a_giant.mp3"
+      mp3:"/portfolio/assets/audio/20150116_me_remembering_a_giant.mp3",
+      intro:'“In his early 20s, Max was in a near-fatal car accident, leaving him quadriplegic and living in a nursing home. One day, he came across a young woman who worked at that nursing home. Her name was Colleen. And at StoryCorps, Colleen sat down with her daughter, Meaghan, to remember Max.”'
     },
     {
       title:"For Veteran, Hospice Care Work Connects Him To Family",
-      mp3:"/portfolio/assets/audio/20141114_me_for_veteran_hospice_care_work_connects_him_to_family.mp3"
+      mp3:"/portfolio/assets/audio/20141114_me_for_veteran_hospice_care_work_connects_him_to_family.mp3",
+      intro:'“Ron Riveira, who served in the Navy and Marines, now does hospice care for vets and says it allows him to help people like his grandparents. \'Every time I go into a home, I see a piece of my family.\'”'
     }
   ], {
     swfPath: "{{ site.baseurl }}",
