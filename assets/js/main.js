25c48d5248e49bfbc35c03355005f9d169aa7708
Emily Martinez <em3021@columbia.edu>
2015-11-19 14:02:03 -0500

    unminifies playlist js, adds intro

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,23 +48,28 @@ $(document).ready(function(){
   }, [
     {
       title:"\'I\'m So Proud To Have You As A Daughter\'",
-            mp3:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter.mp3"
+      mp3:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter.mp3",
+      intro:"intro"
     },
     {
       title:"Dad Aches For Son Killed By Policeman 20 Years Ago",
-      mp3:"/portfolio/assets/audio/20150515_me_dad_aches_for_tween_son_killed_by_policeman_20_years_ago.mp3"
+      mp3:"/portfolio/assets/audio/20150515_me_dad_aches_for_tween_son_killed_by_policeman_20_years_ago.mp3",
+      intro:"intro"
     },
     {
       title:"10 Years Later, A Pair Of Strangers Revisit A Leap Not Taken",
-      mp3:"/portfolio/assets/audio/20150306_me_ten_years_later_two_strangers_revisit_what_might_have_been_lost.mp3"
+      mp3:"/portfolio/assets/audio/20150306_me_ten_years_later_two_strangers_revisit_what_might_have_been_lost.mp3",
+      intro: "intro"
     },
     {
       title:"Remembering A \'Giant\': \'Everything We Did, We Did Together\'",
-      mp3:"/portfolio/assets/audio/20150116_me_remembering_a_giant.mp3"
+      mp3:"/portfolio/assets/audio/20150116_me_remembering_a_giant.mp3",
+      intro: "intro"
     },
     {
       title:"For Veteran, Hospice Care Work Connects Him To Family",
-      mp3:"/portfolio/assets/audio/20141114_me_for_veteran_hospice_care_work_connects_him_to_family.mp3"
+      mp3:"/portfolio/assets/audio/20141114_me_for_veteran_hospice_care_work_connects_him_to_family.mp3",
+      intro: "intro"
     }
   ], {
     swfPath: "{{ site.baseurl }}",
