25131379a85deaa5b75a705710c904d4ff526b81
Emily Martinez <em3021@columbia.edu>
2015-11-25 17:02:00 -0500

    edits header size, background position

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -118,6 +118,7 @@ $(document).ready(function(){
     {
       title:"&#8216;I&#8217;m So Proud To Have You As A Daughter&#8217;",
       mp3:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter.mp3",
+      ogg:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter_01.ogg",
       intro:'“An immigrant from Haiti talks with his high-IQ daughter about his love for her, and why he works so hard to provide for her education.”',
       link:"http://www.npr.org/2015/06/19/415461277/im-so-proud-to-have-you-as-a-daughter",
       dek:"I pitched this story as an intern at StoryCorps and co-facilitated the interview with Nicholas Heyward Sr. at StoryCorps/’ lower Manhattan recording booth."
@@ -125,6 +126,7 @@ $(document).ready(function(){
     {
       title:"Dad Aches For Son Killed By Policeman 20 Years Ago",
       mp3:"/portfolio/assets/audio/20150515_me_dad_aches_for_tween_son_killed_by_policeman_20_years_ago.mp3",
+      ogg:"/portfolio/assets/audio/20150515_me_dad_aches_for_tween_son_killed_by_policeman_20_years_ago_01.ogg",
       intro:'“Thirteen-year-old Nicholas Heyward Jr. was playing with a toy gun in the stairwell of the housing complex where he lived in Brooklyn when a police officer shot and killed him in 1994.”',
       link:"http://www.npr.org/2015/05/15/406735395/dad-aches-for-tween-son-killed-by-policeman-20-years-ago",
       dek:"I pitched this story as an intern at StoryCorps and co-facilitated the interview with Nicholas Heyward Sr. at StoryCorps’ lower Manhattan recording booth."
@@ -132,6 +134,7 @@ $(document).ready(function(){
     {
       title:"10 Years Later, A Pair Of Strangers Revisit A Leap Not Taken",
       mp3:"/portfolio/assets/audio/20150306_me_ten_years_later_two_strangers_revisit_what_might_have_been_lost.mp3",
+      ogg:"/portfolio/assets/audio/20150306_me_ten_years_later_two_strangers_revisit_what_might_have_been_lost_01.ogg",
       intro:'“On March 11, 2005, Kevin Berthia wanted to jump off the Golden Gate Bridge. California Highway Patrol Officer Kevin Briggs talked him out of it. The two met recently to discuss what happened that day.”',
       link:"http://www.npr.org/2015/03/06/390970491/ten-years-later-two-strangers-revisit-what-might-have-been-lost",
       dek:"I pitched this story as an intern at StoryCorps and booked the interviews with Kevin Briggs and Kevin Berthia."
@@ -139,6 +142,7 @@ $(document).ready(function(){
     {
       title:"Remembering A &#8216;Giant&#8217;: &#8216;Everything We Did, We Did Together&#8217;",
       mp3:"/portfolio/assets/audio/20150116_me_remembering_a_giant.mp3",
+      ogg:"/portfolio/assets/audio/20150116_me_remembering_a_giant_01.ogg",
       intro:'“In his early 20s, Max was in a near-fatal car accident, leaving him quadriplegic and living in a nursing home. One day, he came across a young woman who worked at that nursing home. Her name was Colleen. And at StoryCorps, Colleen sat down with her daughter, Meaghan, to remember Max.”',
       link:"http://www.npr.org/2015/01/16/377450795/remembering-a-giant-everything-we-did-we-did-together",
       dek:"I contributed to the production of this piece by creating the first edit of the original 40-minute interview between Colleen and Meaghan Starkloff."
@@ -146,6 +150,7 @@ $(document).ready(function(){
     {
       title:"For Veteran, Hospice Care Work Connects Him To Family",
       mp3:"/portfolio/assets/audio/20141114_me_for_veteran_hospice_care_work_connects_him_to_family.mp3",
+      ogg:"/portfolio/assets/audio/20141114_me_for_veteran_hospice_care_work_connects_him_to_family_01.ogg",
       intro:'“Ron Riveira, who served in the Navy and Marines, now does hospice care for vets and says it allows him to help people like his grandparents. \'Every time I go into a home, I see a piece of my family.\'”',
       link:"http://www.npr.org/2014/11/14/363762702/for-veteran-hospice-care-work-connects-him-to-family",
       dek:"I contributed to the production of this piece by creating the first edit of the original 40-minute interview with Ron Riveira."
