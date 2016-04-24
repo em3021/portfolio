2c0c52ac17736fd42c8d38c5869cf43a1cad8ca5
Emily Martinez <em3021@columbia.edu>
2016-04-24 18:02:52 -0400

    Adds de la cruz piece

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,12 +119,20 @@ function hasScrolled() {
     cssSelectorAncestor: "#jp_container_1"
   }, [
     {
+      title:" &#8216;I Can Smell The Dirt And The Fear&#8217;: Up From Soil, The Soul Of A Law Career",
+      mp3:"/portfolio/assets/audio/20160422_me_i_can_smell_the_dirt_and_the_fear_up_from_soil_the_soul_of_a_law_career.mp3",
+      ogg:"/portfolio/assets/audio/20160422_me_i_can_smell_the_dirt_and_the_fear_up_from_soil_the_soul_of_a_law_career_01.ogg",
+      intro:'“Vito de la Cruz grew up in a family of migrant farm workers in the 1960s. And, though the civil rights lawyer is now far from those fields, he\'s still motivated by the memory of a brutal day.”',
+      link:"http://www.npr.org/2016/04/22/475130720/i-can-smell-the-dirt-and-the-fear-up-from-soil-the-soul-of-a-law-career",
+      dek:"I contributed to the production of this piece by creating the first edit of the original 40-minute interview using Pro Tools. I then went through several stages of editing the piece with our executive producer, Michael Garofalo, until it was approved by StoryCorps founder, Dave Isay, and editors at NPR\'s Morning Edition. Lastly, I chose the music and mixed the piece. "
+    },
+    {
       title:"&#8216;I&#8217;m So Proud To Have You As A Daughter&#8217;",
       mp3:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter.mp3",
       ogg:"/portfolio/assets/audio/20150619_me_im_so_proud_to_have_you_as_a_daughter_01.ogg",
       intro:'“An immigrant from Haiti talks with his high-IQ daughter about his love for her, and why he works so hard to provide for her education.”',
       link:"http://www.npr.org/2015/06/19/415461277/im-so-proud-to-have-you-as-a-daughter",
-      dek:"I pitched this story as an intern at StoryCorps and co-facilitated the interview with Nicholas Heyward Sr. at StoryCorps’ lower Manhattan recording booth."
+      dek:"I pitched this story as an intern at StoryCorps and co-facilitated the interview with participants Mabou Loiseau and her father Mario Loiseau at StoryCorps’ lower Manhattan recording booth."
     },
     {
       title:"Dad Aches For Son Killed By Policeman 20 Years Ago",
@@ -132,7 +140,7 @@ function hasScrolled() {
       ogg:"/portfolio/assets/audio/20150515_me_dad_aches_for_tween_son_killed_by_policeman_20_years_ago_01.ogg",
       intro:'“Thirteen-year-old Nicholas Heyward Jr. was playing with a toy gun in the stairwell of the housing complex where he lived in Brooklyn when a police officer shot and killed him in 1994.”',
       link:"http://www.npr.org/2015/05/15/406735395/dad-aches-for-tween-son-killed-by-policeman-20-years-ago",
-      dek:"I pitched this story as an intern at StoryCorps and co-facilitated the interview with Nicholas Heyward Sr. at StoryCorps’ lower Manhattan recording booth."
+      dek:"I pitched this story as an intern at StoryCorps and co-facilitated the interview with Nicholas Heyward Sr. at StoryCorps’ lower Manhattan recording booth. I also worked with producer Jud Esty-Kendall on editing the narrative structure of the piece."
     },
     {
       title:"10 Years Later, A Pair Of Strangers Revisit A Leap Not Taken",
